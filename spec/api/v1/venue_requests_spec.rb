0052bf066eb652674c0666825d3b45f0dc9ddb7f
kallaugher <alicekallaugher@gmail.com>
2016-09-16 13:26:42 -0400

    add venue requests spec file

---
+++ spec/api/v1/venue_requests_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe 'Venue API' do
+  describe 'get /venues' do
+
+  end
+
+  describe 'get /venues/:id' do
+
+  end
+
+  describe 'post /venues' do
+
+  end
+
+  describe 'patch /venues/:id' do
+
+  end
+
+  describe 'destroy /venues/:id' do
+    
+  end
+end
