28b735ad0bdfdaca5e0d54b4c7df6c7ccfb542bc
kallaugher <alicekallaugher@gmail.com>
2016-09-16 13:35:29 -0400

    add venues controller and action

--- spec/api/v1/venue_requests_spec.rb
+++ spec/api/v1/venue_requests_spec.rb
@@ -2,7 +2,15 @@ require 'rails_helper'
 
 describe 'Venue API' do
   describe 'get /venues' do
-
+    it 'returns a JSON collection of all the venues' do
+      Venue.create(name: 'Mobtown Ballroom', location: 'Baltimore', capacity: 200)
+      Venue.create(name: 'Madison Square Garden', location: 'New York City', capacity: 30000)
+      get '/api/v1/venues'
+      response_body = JSON.parse(response.body)
+      expect(response).to be_success
+      expect(response_body.length).to eq(2)
+      expect(response_body.first["performers"].to eq([]))
+    end
   end
 
   describe 'get /venues/:id' do
@@ -18,6 +26,6 @@ describe 'Venue API' do
   end
 
   describe 'destroy /venues/:id' do
-    
+
   end
 end
