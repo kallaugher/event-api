fe8ed16833cb9dbda491d05be48e4266610eefa2
Alison <alisonhmullin@gmail.com>
2016-09-16 13:18:46 -0400

    add performer and booking tests

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :venue
   belongs_to :performer
+
+  validates :date, presence: true
 end
--- app/models/performer.rb
+++ app/models/performer.rb
@@ -1,4 +1,7 @@
 class Performer < ApplicationRecord
   has_many :bookings
   has_many :venues, through: :bookings
+
+  validates :name, presence: true
+
 end
